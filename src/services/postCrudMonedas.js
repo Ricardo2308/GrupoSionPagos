dd88a2c2535e87e9f0ed60c0430dbc8db6385aab
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-08-14 00:17:09 -0600

    Add files via upload

--- src/services/postCrudMonedas.js
+++ src/services/postCrudMonedas.js
@@ -1,15 +1,24 @@
-const API = `${process.env.REACT_APP_BACKEND_URL}post_monedas.php`
+const API = `${process.env.REACT_APP_API_URL}monedas`
 
 export function postCrudMonedas(idMoneda, nombre, simbolo, estado, opcion) {
+  let ApiFinal = API
+  let ApiWhere = ''
+
   var datos = {
     id_moneda: idMoneda,
     nombre: nombre,
     simbolo: simbolo,
-    estado: estado,
+    activo: estado,
     opcion: opcion,
   }
+
+  if (idMoneda !== '') {
+    ApiWhere += '/' + idMoneda
+  }
+  ApiFinal += ApiWhere
+
   const data = JSON.stringify(datos)
-  return fetch(API, {
+  return fetch(ApiFinal, {
     method: 'POST',
     body: data,
     headers: {
