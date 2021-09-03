6447c64b2348c3d1764e8198257ec2bd1aaa127d
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-09-02 21:56:45 -0600

    Add files via upload

---
+++ src/services/postFlujoDetalle.js
@@ -0,0 +1,25 @@
+const API = `${process.env.REACT_APP_API_URL}flujodetalle`
+
+export function postFlujoDetalle(idFlujo, idEstadoFlujo, idUsuario, Comentario) {
+  var datos = {
+    IdFlujo: idFlujo,
+    IdEstadoFlujo: idEstadoFlujo,
+    IdUsuario: idUsuario,
+    Comentario: Comentario,
+  }
+
+  const data = JSON.stringify(datos)
+  return fetch(API, {
+    method: 'POST',
+    body: data,
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json;charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      return response
+    })
+    .catch((error) => error)
+}
---
+++ src/services/postFlujoGrupo.js
@@ -0,0 +1,34 @@
+const API = `${process.env.REACT_APP_API_URL}flujogrupo`
+
+export function postFlujoGrupo(idFlujoGrupo, idFlujo, idGrupo, estado, opcion) {
+  let ApiFinal = API
+  let ApiWhere = ''
+
+  var datos = {
+    id_flujogrupo: idFlujoGrupo,
+    id_flujo: idFlujo,
+    id_grupoautorizacion: idGrupo,
+    activo: estado,
+    opcion: opcion,
+  }
+
+  if (idFlujoGrupo !== '' && opcion !== '') {
+    ApiWhere += '/' + idFlujoGrupo + '/' + opcion
+  }
+  ApiFinal += ApiWhere
+
+  const data = JSON.stringify(datos)
+  return fetch(ApiFinal, {
+    method: 'POST',
+    body: data,
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json;charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      return response
+    })
+    .catch((error) => error)
+}
