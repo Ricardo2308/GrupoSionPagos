b2caa86898f647d09ed693f07d75ff8a8ec6e6d7
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-09-29 17:04:18 -0600

    Add files via upload

--- src/services/getFlujos.js
+++ src/services/getFlujos.js
@@ -1,10 +1,10 @@
 const API = `${process.env.REACT_APP_API_URL}flujos`
 
-export function getFlujos(idFlujo, Tipo, idUsuario, opcion) {
+export function getFlujos(idFlujo, Tipo, idUsuario, opcion, year, mes) {
   let ApiFinal = API
   let ApiWhere = ''
-  if (idFlujo !== null && opcion !== null) {
-    ApiWhere += '/' + opcion + '/' + idFlujo
+  if (idFlujo !== null && opcion !== null && year !== null && year !== null) {
+    ApiWhere += '/' + opcion + '/' + idFlujo + '/' + year + '/' + mes
   }
   if (Tipo !== null) {
     ApiWhere += '/0/' + Tipo + '/' + idUsuario
