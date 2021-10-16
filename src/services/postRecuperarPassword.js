1e2c966b4f4929b169369ae2fa89337295438f92
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-10-15 23:44:16 -0600

    Add files via upload

---
+++ src/services/postRecuperarPassword.js
@@ -0,0 +1,22 @@
+const API = `${process.env.REACT_APP_API_URL}recuperapassword`
+
+export function postRecuperarPassword(usuario) {
+  var datos = {
+    usuario: usuario,
+  }
+
+  const data = JSON.stringify(datos)
+  return fetch(API, {
+    method: 'POST',
+    body: data,
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json;charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      return response
+    })
+    .catch((error) => error)
+}
