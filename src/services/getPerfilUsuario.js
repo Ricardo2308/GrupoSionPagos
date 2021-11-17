9b15709aa570c73b255e95acb0f0805807e28574
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-11-16 23:40:47 -0600

    Add files via upload

--- src/services/getPerfilUsuario.js
+++ src/services/getPerfilUsuario.js
@@ -1,33 +1,21 @@
 const API = `${process.env.REACT_APP_API_URL}usuarioperfil`
 
-export function getPerfilUsuario(idUsuario, opcion, objetos) {
-  let array = ['0']
-
-  if (objetos !== null) {
-    var datos = {
-      objetos: objetos,
-      opcion: opcion,
-      idUsuario: idUsuario,
-    }
-  } else {
-    var datos = {
-      objetos: array,
-      opcion: opcion,
-      idUsuario: idUsuario,
-    }
+export function getPerfilUsuario(idUsuario, opcion, objeto) {
+  let ApiFinal = API
+  let ApiWhere = ''
+  if (idUsuario !== null) {
+    ApiWhere += '/' + idUsuario
   }
-  const data = JSON.stringify(datos)
-  return fetch(API, {
-    method: 'POST',
-    body: data,
-    headers: {
-      Accept: 'application/json',
-      'Content-type': 'application/json;charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      return response
+  if (objeto !== null) {
+    ApiWhere += '/' + objeto
+  }
+  if (opcion !== null) {
+    ApiWhere += '/' + opcion
+  }
+  ApiFinal += ApiWhere
+  return fetch(ApiFinal)
+    .then(function (response) {
+      return response.json()
     })
     .catch((error) => error)
 }
