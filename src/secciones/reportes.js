5fd1743739a264a90906274f830447f3580c25ed
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2021-11-18 22:55:15 -0600

    Add files via upload

--- src/secciones/reportes.js
+++ src/secciones/reportes.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { FiCreditCard } from 'react-icons/fi'
-import { FaRegListAlt } from 'react-icons/fa'
+import { FaRegListAlt, FaRegChartBar } from 'react-icons/fa'
 
-const reportes = [
-  {
-    _component: 'CNavItem',
-    as: NavLink,
-    anchor: 'Pagos Pendientes',
-    icon: <FiCreditCard size={16} style={{ marginRight: '4px' }} />,
-    to: '/reportependientes',
-  },
-  {
-    _component: 'CNavItem',
-    as: NavLink,
-    anchor: 'Días Conectados',
-    icon: <FaRegListAlt size={16} style={{ marginRight: '4px' }} />,
-    to: '/to',
-  },
-]
+const reportes = {
+  _component: 'CNavGroup',
+  as: NavLink,
+  anchor: 'Reportes',
+  to: '/to',
+  icon: <FaRegChartBar size={20} style={{ marginRight: '20px', marginLeft: '7px' }} />,
+  items: [
+    {
+      _component: 'CNavItem',
+      as: NavLink,
+      anchor: 'Pagos Pendientes',
+      icon: <FiCreditCard size={16} style={{ marginRight: '4px' }} />,
+      to: '/reportependientes',
+    },
+    {
+      _component: 'CNavItem',
+      as: NavLink,
+      anchor: 'Días Conectados',
+      icon: <FaRegListAlt size={16} style={{ marginRight: '4px' }} />,
+      to: '/reportependientes',
+    },
+  ],
+}
 
 export default reportes
