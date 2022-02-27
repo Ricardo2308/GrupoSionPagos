7f28d67859bebe482875216a770e6330817a7303
Ricardo2308 <43019105+Ricardo2308@users.noreply.github.com>
2022-02-27 12:49:13 -0600

    Add files via upload

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -284,19 +284,29 @@ const Dashboard = () => {
                       {
                         backgroundColor: [
                           '#D02F2F',
-                          '#ADBC3C',
+                          '#AF940B',
+                          '#428A49',
                           '#40389D',
                           '#8A5C84',
-                          '#428A49',
                           '#553D26',
+                          '#CD530F',
+                          '#454143',
+                          '#0B4B4E',
+                          '#7D1843',
+                          '#6B9614',
                         ],
                         hoverBackgroundColor: [
                           '#CC5855',
                           '#C7C246',
+                          '#56A05A',
                           '#6F72C5',
                           '#8E6BC2',
-                          '#56A05A',
                           '#825E3D',
+                          '#CF7340',
+                          '#676768',
+                          '#18787D',
+                          '#87405E',
+                          '#9DCA42',
                         ],
                         data: results,
                       },
@@ -316,8 +326,8 @@ const Dashboard = () => {
                     datasets: [
                       {
                         data: pagos,
-                        backgroundColor: ['#D02F2F', '#40389D', '#428A49'],
-                        hoverBackgroundColor: ['#CC5855', '#6F72C5', '#56A05A'],
+                        backgroundColor: ['#D02F2F', '#AF940B', '#428A49'],
+                        hoverBackgroundColor: ['#CC5855', '#C7C246', '#56A05A'],
                       },
                     ],
                   }}
